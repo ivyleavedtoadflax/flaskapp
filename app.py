5f96e8723156e80595fb95d84585151ccd3db927
Matthew Upson <matthew.upson@digital.cabinet-office.gov.uk>
2017-11-17 16:08:51 +0000

    Correct mistakes

--- app.py
+++ app.py
@@ -1,7 +1,9 @@
 """
-Simple DIY blockchain from:
+Simple flask app
 """
-from flask import Flask,
+from flask import Flask
+
+app = Flask(__name__)
 
 @app.route('/', methods=['GET'])
 print('Hello World')
