fe8198ffa34c470d246a90c7d4c4cf87eac70e09
Matthew Upson <matthew.upson@digital.cabinet-office.gov.uk>
2017-11-17 16:04:58 +0000

    Initial commit

---
+++ app.py
@@ -0,0 +1,10 @@
+"""
+Simple DIY blockchain from:
+"""
+from flask import Flask,
+
+@app.route('/', methods=['GET'])
+print('Hello World')
+
+if __name__ == '__main__':
+    app.run(debug=True, use_reloader=True)
